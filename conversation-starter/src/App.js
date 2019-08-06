b81f252f8f7abcf46e522ab054cb09e72533b392
Brandon Weygant <bobbydigital108@gmail.com>
2019-08-06 15:13:18 -0400

    Added YouContainer to project.

--- conversation-starter/src/App.js
+++ conversation-starter/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './components/Home.js'
+import YouContainer from './containers/YouContainer.js'
 
 function App() {
   return (
     <Router>
     <div className="App">
-      <Route path "/" component={Home}
+      <Route exact path="/" component={Home} />
+      <Route path="/you" component={YouContainer} />
     </div>
     </Router>
   );
