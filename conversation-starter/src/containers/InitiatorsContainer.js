ab4d57546b98a28d954aa1b6e345a7fa311ab63c
BMWeygant <bobbydigital108@gmail.com>
2019-08-08 23:39:20 -0400

    IDK. I made this commit several hours after I actually did the work I am just now pushing as I lay awake stressed at how far I am behind on my final project.

--- conversation-starter/src/containers/InitiatorsContainer.js
+++ conversation-starter/src/containers/InitiatorsContainer.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux';
+import { connect } from 'react-redux'
+import { fetchInitiators } from '../redux/actions/initiatorsActions'
 
  class InitiatorsContainer extends Component {
-    state = {
-        initiators: []
-    }
+    
 
     componentDidMount() {
-        fetch("http://localhost:3001/initiators")
-        .then(res => res.json())
-        .then(initiators => this.setState( { initiators }))
+        this.props.fetchInitiators()
     }
+
     render(){
         if(this.props.initiators.length === 0){
         return <h1>Just going through motions</h1>
         }
          return (
-            <div>
+            <div >
                 <ul>
                     {this.props.initiators.map(initiator => (
-                        <div>
-                            <p>ID: {initiator.id}</p>
+                        <div key={initiator.id}>
+                            
                             <p>Gender: {initiator.gender}</p>
                             <p>Swag: {initiator.swag}</p>
                             <p>Conscience: {initiator.conscience}</p>
@@ -39,4 +37,4 @@ const mapStateToProps = state => {
         initiators: state.initiators
     }
 }
-export default connect(mapStateToProps)(InitiatorsContainer)
+export default connect(mapStateToProps, { fetchInitiators })(InitiatorsContainer)
--- conversation-starter/src/redux/reducers/initiatorsReducer.js
+++ conversation-starter/src/redux/reducers/initiatorsReducer.js
@@ -1,5 +1,13 @@
 const initialState = []
 
+// const initialState = {
+//     gender: '',
+//     swag: '',
+//     conscience: null,
+//     romantic_interest: true,
+//     initiators: []
+// }
+
 export default (state = initialState, action) => {
     switch(action.type){
         case "FETCH_INITIATORS_FINISHED":
