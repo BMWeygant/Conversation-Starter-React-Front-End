3c15bddda15eedb2457898e199ae819ebc9cef21
Brandon Weygant <bobbydigital108@gmail.com>
2019-08-13 13:12:49 -0400

    Imported convosReducer to redux index.js

--- conversation-starter/src/redux/index.js
+++ conversation-starter/src/redux/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 // import initiators from './reducers/initiatorsReducer'
+import convos from './reducers/convosReducer'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
-    initiators
+    //initiators
+    convos
 })
 
 //const middleware = (thunk)
