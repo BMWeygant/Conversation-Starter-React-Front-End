83c47a2facbfe585899e0d93c7183bc48305e028
BMWeygant <bobbydigital108@gmail.com>
2019-08-16 14:01:08 -0400

    Added code to ConvosContainer.

---
+++ conversation-starter/src/containers/ConvosContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+export default class ConvosContainer extends Component {
+    state = {
+        convos: []
+    }
+
+    componentDidMount() {
+        fetch("http://localhost:3001/convos")
+        .then(res => res.json())
+        .then(convos => this.setState( { convos }))
+    }
+
+    render(){
+        if(this.state.convos.length === 0) {
+            return <h1>Loading</h1>
+        }
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
