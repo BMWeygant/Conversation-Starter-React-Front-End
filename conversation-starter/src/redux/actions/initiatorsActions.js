63d0f634004d8fb86bec610e61cf2ef39e423590
BMWeygant <bobbydigital108@gmail.com>
2019-08-16 12:37:09 -0400

    Deleted unnecessarry initiators files.

--- conversation-starter/src/redux/actions/initiatorsActions.js
+++
@@ -1,7 +0,0 @@
-// export const fetchInitiators = () => dispatch => {
-//     return fetch("http://localhost:3001/initiators")
-//     .then(res => res.json())
-//     .then(initiators =>
-//       dispatch({type: "FETCH_INITIATORS_FINISHED", payload: initiators})
-//     )
-//   }
