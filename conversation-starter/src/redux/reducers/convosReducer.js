ce669a5ed8ddc9f5eb952b3a73fa960c034d2d34
BMWeygant <bobbydigital108@gmail.com>
2019-08-16 12:49:02 -0400

    Removed code from convosReducer.

--- conversation-starter/src/redux/reducers/convosReducer.js
+++ conversation-starter/src/redux/reducers/convosReducer.js
@@ -1,44 +1,12 @@
-const initialState = {
-        initiator_gender: '',
-        initiator_swag: '',
-        target_gender: '',
-        target_approachability: '',
-        relationship: '',
-        topic: '',
-        location: '',
-        conscience: '',
-        ice_breaker: '',
-    }
-    
-    export default (state = initialState, action) => {
-        switch(action.type){
-           case 'FETCH_CONVOS_FINISHED':
-               return {
-                ...state,
-                initiators: action.payload,
-               }
-            case 'FETCH_GENDER_FINISHED':
-               return {
-                ...state,
-                initiators: action.payload,
-               }
-            case 'FETCH_SWAG_FINISHED':
-               return {
-                ...state,
-                initiators: action.payload,
-               }
-            case 'FETCH_CONSCIENCE_FINISHED':
-               return {
-                ...state,
-                initiators: action.payload,
-               }
-            case 'FETCH_ROMANTIC_INTEREST_FINISHED':
-               return {
-                ...state,
-                initiators: action.payload,
-               }
-            default:
-                return state
-        }
-    }
+// const initialState = {
+//         initiator_gender: '',
+//         initiator_swag: '',
+//         target_gender: '',
+//         target_approachability: '',
+//         relationship: '',
+//         topic: '',
+//         location: '',
+//         conscience: '',
+//         ice_breaker: '',
+//     }
     
