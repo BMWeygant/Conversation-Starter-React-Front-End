c74595d10d49afb6ab06ef5b0f955dd7f202ff3e
BMWeygant <bobbydigital108@gmail.com>
2019-08-19 10:49:19 -0400

    Changed return values to be more comprehensive.

--- conversation-starter/src/redux/reducers/convosReducer.js
+++ conversation-starter/src/redux/reducers/convosReducer.js
@@ -1,15 +1,36 @@
-const initialState = []
+const initialState = {
+  relationship: '',
+  location: '',
+  approachability: '',
+  quote: ''
+}
     
     export default (state = initialState, action) => {
        switch(action.type){
           case "FETCH_RELATIONSHIP_SUCCESS":
-             return action.payload
+             return {
+               ...state, 
+               relationship: action.payload,
+            }
           case "FETCH_LOCATION_SUCCESS":
-               return action.payload
+               return {
+               ...state, 
+               location: action.payload,
+            }
           case "FETCH_APPROACHABILITY_SUCCESS":
-               return action.payload
-          case "FETCH_QUOTE_SUCCESS":
-               return action.payload;
+               return {
+               ...state, 
+               approachability: action.payload,
+            }         
+             case "FETCH_QUOTE_SUCCESS":
+               return {
+               ...state, 
+               quote: action.payload,
+            }           
+            case "START_NEW_CONVO":
+              return {
+                 initialState
+               }
           default:
              return state
        }
