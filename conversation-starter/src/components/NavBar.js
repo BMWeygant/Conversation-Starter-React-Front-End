534e76f0f19d87258c6af38ab4ee419d41746895
BMWeygant <bobbydigital108@gmail.com>
2019-08-19 15:01:23 -0400

    Added  link to new convo.

--- conversation-starter/src/components/NavBar.js
+++ conversation-starter/src/components/NavBar.js
@@ -5,7 +5,8 @@ const NavBar = () => {
     return (
         <div className="navigationWrapper">
             <div className="linksWrapper">
-                <Link to = '/' className = 'navLink' activeclassname="activeLink">Home </Link>
+                <Link to = '/' className = 'navLink' activeclassname="activeLink">Home</Link> <br />
+                <Link to = '/convos' className = 'navLink' activeclassname="activeLink">New Conversation</Link>
                 {/* <Link to = '/initiators' className = 'navLink' activeclassname="activeLink">Initiators </Link> */}
                 {/* <Link to = '/targets' className = 'navLink' activeclassname="activeLink">Targets </Link> */}
             </div>
