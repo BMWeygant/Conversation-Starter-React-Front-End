76ae3f0432d61a6fe4a33f3bbb6d4ced5715f491
BMWeygant <bobbydigital108@gmail.com>
2019-08-20 14:16:58 -0400

    Updated unused initiators link to about link.

--- conversation-starter/src/components/NavBar.js
+++ conversation-starter/src/components/NavBar.js
@@ -6,8 +6,8 @@ const NavBar = () => {
         <div className="navigationWrapper">
             <div className="linksWrapper">
                 <Link to = '/' className = 'navLink' activeclassname="activeLink">Home</Link> <br />
-                <Link to = '/convos' className = 'navLink' activeclassname="activeLink">New Conversation</Link>
-                {/* <Link to = '/initiators' className = 'navLink' activeclassname="activeLink">Initiators </Link> */}
+                <Link to = '/convos' className = 'navLink' activeclassname="activeLink">Conversation</Link>
+                <Link to = '/about' className = 'navLink' activeclassname="activeLink">About Me </Link>
                 {/* <Link to = '/targets' className = 'navLink' activeclassname="activeLink">Targets </Link> */}
             </div>
         </div>
