47b6ffd781648950d82518b986549f93e98b320c
BMWeygant <bobbydigital108@gmail.com>
2019-08-20 14:16:34 -0400

    removed commented out img from Home.js

--- conversation-starter/src/components/Home.js
+++ conversation-starter/src/components/Home.js
@@ -1,5 +1,3 @@
 import React from 'react'
 
-export default () => <h1>Welcome, we rolling now!</h1>
-
-//<img src="https://previews.123rf.com/images/tawng/tawng1104/tawng110400004/9235025-editable-silhouettes-of-people-socializing-at-a-party.jpg" alt="background"></img>
+export default () => <h1>Welcome to Conversation Starter!</h1>
