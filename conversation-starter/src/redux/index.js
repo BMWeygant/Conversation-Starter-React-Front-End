2539449d2cc18ef1e82b8a7898159ae2d6994775
BMWeygant <bobbydigital108@gmail.com>
2019-08-20 09:45:06 -0400

    Added quotes to combineReducers

--- conversation-starter/src/redux/index.js
+++ conversation-starter/src/redux/index.js
@@ -1,10 +1,12 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import convos from './reducers/convosReducer'
+import quotes from './reducers/quotesReducer'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
-    convos
+    convos,
+    quotes
 })
 
 //const middleware = (thunk)
