a7b087e9189f602adcfb18583f9edfe51af8c8b0
BMWeygant <bobbydigital108@gmail.com>
2019-08-20 14:49:09 -0400

    Imported About component

--- conversation-starter/src/App.js
+++ conversation-starter/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Home from './components/Home'
+import About from './components/About'
 import NavBar from './components/NavBar'
 import ConvosContainer from './containers/ConvosContainer'
 import QuotesContainer from './containers/QuotesContainer'
