51a29ca818d9e0870165ecbcd68f6d0e6ae737be
BMWeygant <bobbydigital108@gmail.com>
2019-09-27 15:14:08 -0400

    Took out callApi function only used for review.

--- conversation-starter/src/containers/Like.js
+++ conversation-starter/src/containers/Like.js
@@ -13,22 +13,11 @@ export default class Like extends Component {
         }
         ))
     }
-
-
-    
-    callApi = () => {
-        //console.log("calling api")
-        // make a fetch request to your rails backend to get a list of anything - quotes and log them to the console here
-         fetch('http://localhost:3001/quotes')
-        .then(response => response.json())
-        .then(quotes => console.log(quotes))
-       
-    }
     
     render(){
        return (
            <div>
-            <button onClick={this.callApi}>like</button>Likes: {this.state.like}
+            <button onClick={this.handleClick}>like</button>Likes: {this.state.like}
          </div>
         )
      }
