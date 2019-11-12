3fa51a45e703d3c20aa0d0feafe0b4efaa6c2304
BMWeygant <bobbydigital108@gmail.com>
2019-11-12 10:02:44 -0500

    Turned logo on home page to a clickable link

--- conversation-starter/src/components/Home.js
+++ conversation-starter/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 export default class Home extends Component {
 
 
@@ -8,7 +8,7 @@ export default class Home extends Component {
       return(
         <div className="home-page">
           <h1>Welcome</h1>
-          <img src= "https://challengingbehavior.cbcs.usf.edu/images/Logos/NCPMI_LetsTalk_logo.png" alt="home-pic"/>
+          <Link to = "/convos" className='logo-link' activeclassname="activeLink"><img src= "https://challengingbehavior.cbcs.usf.edu/images/Logos/NCPMI_LetsTalk_logo.png" alt="home-pic"/></Link>
         </div>
 
       )
