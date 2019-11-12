91dcd25454db97b3207169b2e5e4dc332b0ec6c2
BMWeygant <bobbydigital108@gmail.com>
2019-11-12 13:11:58 -0500

    Changed app css for more professional look.

--- conversation-starter/src/components/NavBar.js
+++ conversation-starter/src/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     return (
         <div className="nav-wrapper">
             <div className="nav-links-wrapper">
-                <Link to = '/' className = 'navLink' activeclassname="activeLink">Home      </Link>
+                <Link to = '/' className = 'logo-navLink' activeclassname="activeLink"><img src= "https://challengingbehavior.cbcs.usf.edu/images/Logos/NCPMI_LetsTalk_logo.png" height='50px' alt="home-pic"/></Link>
                 <Link to = '/convos' className = 'navLink' activeclassname="activeLink">Conversation       </Link>
                 <Link to = '/about' className = 'navLink' activeclassname="activeLink">About Me </Link>
             </div>
